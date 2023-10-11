6342f35aefbbe9aec2cad34e5f7687f8b6e46e96
__ysfk <21014438+YusufKocyigit@users.noreply.github.com>
2023-10-11 15:59:16 +0200

    hides posts with keywords working

--- content.js
+++ content.js
@@ -10,8 +10,19 @@ chrome.storage.sync.get({ keywords: [] }, function (result) {
 
     for (const keyword of keywords) {
       if (titleText.includes(keyword)) {
-        heading.style.backgroundColor = "red";
-        console.log("Highlighted heading: " + headingText);
+        // Find the nearest common ancestor and hide it by setting display to "none"
+        let parentElement = heading;
+        for (let i = 0; i < 7; i++) {
+          if (parentElement.parentElement) {
+            parentElement = parentElement.parentElement;
+          } else {
+            break; // Reached the top of the DOM tree
+          }
+        }
+        if (parentElement) {
+          parentElement.style.display = "none";
+          console.log("Hidden post with heading: " + headingText);
+        }
       }
     }
   }
