992d02c1ad803201cae21e67dec2d311f4ddd2f8
__ysfk <21014438+YusufKocyigit@users.noreply.github.com>
2023-10-15 21:07:16 +0200

    better working

--- content.js
+++ content.js
@@ -2,28 +2,49 @@ console.log("Content Script Loaded");
 
 chrome.storage.sync.get({ keywords: [] }, function (result) {
   const keywords = result.keywords;
-  const headings = document.querySelectorAll("h3"); // Select all h3 elements
 
-  for (const heading of headings) {
+  // Function to check and highlight keywords in an h3 element
+  function checkAndHighlight(heading) {
     const headingText = heading.textContent;
     const titleText = headingText.toLowerCase();
-
+    
     for (const keyword of keywords) {
       if (titleText.includes(keyword)) {
-        // Find the nearest common ancestor and hide it by setting display to "none"
         let parentElement = heading;
-        for (let i = 0; i < 7; i++) {
+        for (let i = 0; i < 5; i++) {
           if (parentElement.parentElement) {
             parentElement = parentElement.parentElement;
           } else {
-            break; // Reached the top of the DOM tree
+            break;
           }
         }
+        
         if (parentElement) {
-          parentElement.style.display = "none";
+          parentElement.style.backgroundColor = "red";
+          // parentElement.style.display = "none";
           console.log("Hidden post with heading: " + headingText);
         }
       }
     }
   }
+
+  // Create a MutationObserver to observe changes in the DOM
+  const observer = new MutationObserver(function (mutationsList) {
+    for (const mutation of mutationsList) {
+      if (mutation.type === "childList") {
+        // Check for new h3 elements in added nodes
+        for (const addedNode of mutation.addedNodes) {
+          if (addedNode instanceof HTMLElement) {
+            const headings = addedNode.querySelectorAll("h3");
+            for (const heading of headings) {
+              checkAndHighlight(heading);
+            }
+          }
+        }
+      }
+    }
+  });
+
+  // Start observing changes in the DOM, including subtree and childList
+  observer.observe(document.body, { childList: true, subtree: true });
 });
